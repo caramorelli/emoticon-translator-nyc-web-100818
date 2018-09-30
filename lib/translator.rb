f98b992b6fd3a33067ef62f30c3765b98333e348
Cara Morelli <caramorelli7@gmail.com>
2018-09-30 20:20:52 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,32 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(lib)
+  library = YAML.load_file(lib) 
+  new_lib = { "get_meaning" => {}, "get_emoticon" => {}}
+  library.each do |key, val|
+    inside_hash = { val[-1] => key}
+    new_lib.merge(new_lib["get_meaning"] = inside_hash)
+    # puts new_lib["get_meaning"]
+  end 
+  puts new_lib.values
+  new_lib
+ 
 end
 
-def get_japanese_emoticon
-  # code goes here
+load_library("./lib/emoticons.yml")
+
+def get_japanese_emoticon(lib, message)
+  library = load_library(lib)
+   library.each do |key, val|
+    return val if val.include?(message)
+  end 
+  return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(lib, message)
+  library = load_library(lib)
+  library.each do |key, val|
+    return key if val.include?(message)
+  end 
+  return "Sorry, that emoticon was not found"
 end
