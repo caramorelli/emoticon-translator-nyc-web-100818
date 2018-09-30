6afea1e206c12b789e78856e6209f7fda5304485
Cara Morelli <caramorelli7@gmail.com>
2018-09-30 20:21:47 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,7 @@ require "pry"
 
 def load_library(lib)
   library = YAML.load_file(lib) 
+  new_lib = {}
   new_lib = { "get_meaning" => {}, "get_emoticon" => {}}
   library.each do |key, val|
     inside_hash = { val[-1] => key}
