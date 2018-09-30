5492f0f2ec952b4a79587a2f7b4d41384fd3ae4b
Cara Morelli <caramorelli7@gmail.com>
2018-09-30 20:30:05 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,3 +29,21 @@ def get_english_meaning(lib, message)
   end 
   return "Sorry, that emoticon was not found"
 end
+
+# def get_japanese_emoticon(path, emoticon)
+#   emoticon_hash = load_library(path)
+#   result = emoticon_hash["get_emoticon"][emoticon]
+#   if result == nil
+#     result = "Sorry, that emoticon was not found" 
+#   end
+#   result
+# end
+
+# def get_english_meaning(path, emoticon)
+#   emoticon_hash = load_library(path)
+#   result = emoticon_hash["get_meaning"][emoticon]
+#   if result == nil
+#     result = "Sorry, that emoticon was not found" 
+#   end
+#   result
+# end
