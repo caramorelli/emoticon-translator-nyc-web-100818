f350642fb483fae75f323af94c2590f1bdee8c92
Cara Morelli <caramorelli7@gmail.com>
2018-09-30 20:27:06 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,21 +15,7 @@ emoticon_hash["get_meaning"][emoticon_set.last] = english_word
  
 end
 
-# def load_library(path)
-
-#   emoticon_hash["get_emoticon"] = Hash.new
-#   emoticon_hash["get_meaning"] = Hash.new
-
-#   emoticons.each do |english_word, emoticon_set|
-#     emoticon_hash["get_emoticon"][sym.first] = sym.last
-#     emoticon_hash["get_meaning"][sym.last] = english_word
-#   end
-#   emoticon_hash
-# end
-
-
-
-load_library("./lib/emoticons.yml")
+# load_library("./lib/emoticons.yml")
 
 def get_japanese_emoticon(lib, message)
   library = load_library(lib)
