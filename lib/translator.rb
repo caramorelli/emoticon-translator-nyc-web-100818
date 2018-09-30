91ddfb0630e3d7bf7cea10863dde6c779c6b9b78
Cara Morelli <caramorelli7@gmail.com>
2018-09-30 20:25:51 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,8 @@ end
 #   emoticon_hash["get_meaning"] = Hash.new
 
 #   emoticons.each do |english_word, emoticon_set|
-#     emoticon_hash["get_emoticon"][sym.first] = emoticon_set.last
+#     emoticon_hash["get_emoticon"][sym.first] = emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+#     emoticon_hash["get_meaning"][emoticon_set.last] = english_word.last
 #     emoticon_hash["get_meaning"][sym.last] = english_word
 #   end
 #   emoticon_hash
